c9589455a1cb8608e583e0fc778225d061f30cec
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2019-01-05 18:45:41 -0300

    BotsApp version 2.0

--- botsapp_2_test.py
+++ botsapp_2_test.py
@@ -12,7 +12,10 @@ print('\n Welcome to BotsApp version 2.0! \n')
 
 # list of spam victims
 contact_list = [
-    "Mariana Quirino"
+    # Enter recipients here:
+    "Person1",
+    "Person2",
+    "Person3"
 ]
 
 input('Press ENTER/RETURN to send spam! <<<SCAN QR CODE FIRST>>>')
@@ -20,24 +23,22 @@ input('Press ENTER/RETURN to send spam! <<<SCAN QR CODE FIRST>>>')
 for victim in contact_list:
     # sets recipient to victim's name
     name = victim
-
     msg = "Do not go gentle into that good night " \
-          "Old age should burn and rave at close of day " \
-          "Rage, rage against the dying of the light " \
-          "Though wise men at their end know dark is right " \
-          "Because their words had forked no lightning they " \
-          "Do not go gentle into that good night " \
-          "Good men, the last wave by, crying how bright " \
-          "Their frail deeds might have danced in a green bay " \
-          "Rage, rage against the dying of the light " \
-          "Wild men who caught and sang the sun in flight " \
-          "And learn, too late, they grieved it on its way " \
-          "Do not go gentle into that good night " \
-          "Grave men, near death, who see with blinding sight " \
-          "Blind eyes could blaze like meteors and be gay " \
-          "Rage, rage against the dying of the light \n 
-          
-          A poem by Dylan Thomas, but sent remotely by BotsApp 2.0 automated SpamBot ;-)"
+        "Old age should burn and rave at close of day " \
+        "Rage, rage against the dying of the light " \
+        "Though wise men at their end know dark is right " \
+        "Because their words had forked no lightning they " \
+        "Do not go gentle into that good night " \
+        "Good men, the last wave by, crying how bright " \
+        "Their frail deeds might have danced in a green bay " \
+        "Rage, rage against the dying of the light " \
+        "Wild men who caught and sang the sun in flight " \
+        "And learn, too late, they grieved it on its way " \
+        "Do not go gentle into that good night " \
+        "Grave men, near death, who see with blinding sight " \
+        "Blind eyes could blaze like meteors and be gay " \
+        "Rage, rage against the dying of the light " + 
+        "\n A poem by Dylan Thomas, but sent remotely by BotsApp 2.0 automated SpamBot ;-)"
 
     # selenium wizardry
     user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
